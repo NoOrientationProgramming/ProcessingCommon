41e9faff1cd393a87469d8aa21c008f8b0ca6fa8
Johannes Natter <office@dsp-crowd.com>
2024-10-02 13:51:24 +0200

    MsWaiting(): Add

---
+++ MsWaiting.cpp
@@ -0,0 +1,98 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 02.10.2024
+
+  Copyright (C) 2024-now Authors and www.dsp-crowd.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "MsWaiting.h"
+#include "LibTime.h"
+
+#define dForEach_ProcState(gen) \
+		gen(StStart) \
+		gen(StMain) \
+
+#define dGenProcStateEnum(s) s,
+dProcessStateEnum(ProcState);
+
+#if 0
+#define dGenProcStateString(s) #s,
+dProcessStateStr(ProcState);
+#endif
+
+using namespace std;
+
+#define LOG_LVL	0
+
+MsWaiting::MsWaiting(uint32_t durationMs)
+	: Processing("MsWaiting")
+	, mStartMs(0)
+	, mDurationMs(durationMs)
+	, mDiffMs(0)
+{
+	mState = StStart;
+}
+
+/* member functions */
+
+Success MsWaiting::process()
+{
+	uint32_t curTimeMs = millis();
+	uint32_t diffMs = curTimeMs - mStartMs;
+	//Success success;
+#if 0
+	dStateTrace;
+#endif
+	switch (mState)
+	{
+	case StStart:
+
+		mStartMs = curTimeMs;
+		mState = StMain;
+
+		break;
+	case StMain:
+
+		mDiffMs = diffMs;
+
+		if (diffMs < mDurationMs)
+			break;
+
+		return Positive;
+
+		break;
+	default:
+		break;
+	}
+
+	return Pending;
+}
+
+void MsWaiting::processInfo(char *pBuf, char *pBufEnd)
+{
+#if 1
+	//dInfo("State\t\t\t%s\n", ProcStateString[mState]);
+	pBuf += progressStr(pBuf, pBufEnd, mDiffMs, mDurationMs);
+#endif
+}
+
+/* static functions */
+
---
+++ MsWaiting.h
@@ -0,0 +1,75 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 02.10.2024
+
+  Copyright (C) 2024-now Authors and www.dsp-crowd.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MS_WAITING_H
+#define MS_WAITING_H
+
+#include "Processing.h"
+
+class MsWaiting : public Processing
+{
+
+public:
+
+	static MsWaiting *create(uint32_t duration)
+	{
+		return new (std::nothrow) MsWaiting(duration);
+	}
+
+protected:
+
+	MsWaiting(uint32_t duration);
+	virtual ~MsWaiting() {}
+
+private:
+
+	MsWaiting() = delete;
+	MsWaiting(const MsWaiting &) = delete;
+	MsWaiting &operator=(const MsWaiting &) = delete;
+
+	/*
+	 * Naming of functions:  objectVerb()
+	 * Example:              peerAdd()
+	 */
+
+	/* member functions */
+	Success process();
+	void processInfo(char *pBuf, char *pBufEnd);
+
+	/* member variables */
+	uint32_t mStartMs;
+	uint32_t mDurationMs;
+	uint32_t mDiffMs;
+
+	/* static functions */
+
+	/* static variables */
+
+	/* constants */
+
+};
+
+#endif
+
