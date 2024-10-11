707694d6b4726144e717991a70a016b1ab680d65
Johannes Natter <office@dsp-crowd.com>
2024-10-11 17:47:46 +0200

    DnsResolving(): Add unimplemented

---
+++ DnsResolving.cpp
@@ -0,0 +1,90 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 11.10.2024
+
+  Copyright (C) 2024-now Authors and www.dsp-crowd.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "DnsResolving.h"
+
+#define dForEach_ProcState(gen) \
+		gen(StStart) \
+		gen(StMain) \
+		gen(StNop) \
+
+#define dGenProcStateEnum(s) s,
+dProcessStateEnum(ProcState);
+
+#if 1
+#define dGenProcStateString(s) #s,
+dProcessStateStr(ProcState);
+#endif
+
+using namespace std;
+
+#define LOG_LVL	0
+
+DnsResolving::DnsResolving()
+	: Processing("DnsResolving")
+	//, mStartMs(0)
+{
+	mState = StStart;
+}
+
+/* member functions */
+
+Success DnsResolving::process()
+{
+	//uint32_t curTimeMs = millis();
+	//uint32_t diffMs = curTimeMs - mStartMs;
+	//Success success;
+#if 0
+	dStateTrace;
+#endif
+	switch (mState)
+	{
+	case StStart:
+
+		return procErrLog(-1, "not implemented yet");
+
+		break;
+	case StMain:
+
+		break;
+	case StNop:
+
+		break;
+	default:
+		break;
+	}
+
+	return Pending;
+}
+
+void DnsResolving::processInfo(char *pBuf, char *pBufEnd)
+{
+#if 1
+	dInfo("State\t\t\t%s\n", ProcStateString[mState]);
+#endif
+}
+
+/* static functions */
+
