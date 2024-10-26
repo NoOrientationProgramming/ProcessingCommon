52432ffb2f7630cec67b727488cc7a2bd61f2bf5
Johannes Natter <office@dsp-crowd.com>
2024-10-26 22:58:22 +0200

    Widget: Beautify

--- widgets-term/Widget.cpp
+++ widgets-term/Widget.cpp
@@ -91,29 +91,29 @@ void Widget::modifierContentSet(const string &str)
 void Widget::strPadCutTo(string &str, size_t width, bool dots, bool padLeft)
 {
 	wstring_convert<codecvt_utf8<char32_t>, char32_t> converter;
-	u32string u32str = converter.from_bytes(str);
-	size_t sz = u32str.size();
+	u32string strUtf = converter.from_bytes(str);
+	size_t sz = strUtf.size();
 	size_t szOld = sz;
 
 	if (sz > width)
-		u32str.erase(width);
+		strUtf.erase(width);
 	else
 	if (width > sz)
 	{
 		if (padLeft)
-			u32str.insert(0, width - sz, ' ');
+			strUtf.insert(0, width - sz, ' ');
 		else
-			u32str.append(width - sz, ' ');
+			strUtf.append(width - sz, ' ');
 	}
 
 	if (szOld > width and width >= 2 and dots)
 	{
-		u32str.pop_back();
-		u32str.pop_back();
-		u32str.push_back('.');
-		u32str.push_back('.');
+		strUtf.pop_back();
+		strUtf.pop_back();
+		strUtf.push_back('.');
+		strUtf.push_back('.');
 	}
 
-	str = converter.to_bytes(u32str);
+	str = converter.to_bytes(strUtf);
 }
 
