1343a32436d2d2049e923594a41356dd6670e29e
Johannes Natter <office@dsp-crowd.com>
2024-10-26 18:33:01 +0200

    Widget: Add

---
+++ widgets-term/Widget.cpp
@@ -0,0 +1,88 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 31.01.2023
+
+  Copyright (C) 2023, Johannes Natter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Widget.h"
+
+using namespace std;
+
+Widget::Widget()
+	: mName("")
+	, mLabel("")
+	, mTag("")
+	, mFocus(false)
+	, mCurrent("")
+	, mModifierFrame("")
+	, mModifierContent("")
+{
+}
+
+bool Widget::focus() const
+{
+	return mFocus;
+}
+
+void Widget::focusSet(bool foc, bool accept)
+{
+	if (foc)
+		mWork = mCurrent;
+
+	if (!foc and accept)
+		mCurrent = mWork;
+
+	mFocus = foc;
+}
+
+const string &Widget::current() const
+{
+	return mCurrent;
+}
+
+// https://en.cppreference.com/w/cpp/language/cast_operator
+Widget::operator string() const
+{
+	return current();
+}
+
+void Widget::currentSet(const string &str)
+{
+	mCurrent = str;
+}
+
+Widget &Widget::operator=(const string &str)
+{
+	mCurrent = str;
+	return *this;
+}
+
+void Widget::modifierFrameSet(const string &str)
+{
+	mModifierFrame = str;
+}
+
+void Widget::modifierContentSet(const string &str)
+{
+	mModifierContent = str;
+}
+
---
+++ widgets-term/Widget.h
@@ -0,0 +1,72 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 31.01.2023
+
+  Copyright (C) 2023, Johannes Natter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <string>
+
+#include "KeyUser.h"
+
+class Widget
+{
+
+public:
+	Widget();
+	virtual ~Widget() {}
+
+	std::string mName;
+	std::string mLabel;
+	std::string mTag;
+
+	bool focus() const;
+	virtual void focusSet(bool foc, bool accept = true);
+
+	virtual const std::string &current() const;
+	virtual operator std::string() const;
+	virtual void currentSet(const std::string &str);
+	virtual Widget &operator=(const std::string &str);
+
+	void modifierFrameSet(const std::string &str);
+	void modifierContentSet(const std::string &str);
+
+	// I/O
+	virtual bool keyProcess(const KeyUser &key, const char *pListKeysDisabled = NULL) = 0;
+	virtual bool print(std::string &str) = 0;
+
+protected:
+
+	bool mFocus;
+	std::string mCurrent;
+	std::string mWork;
+	std::string mModifierFrame;
+	std::string mModifierContent;
+
+private:
+
+};
+
+#endif
+
